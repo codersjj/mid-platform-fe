3f17e8d87ada8c1cfc9321d83e3010d9072ff5e0
codersjj <shanewestlife@outlook.com>
2024-01-14 19:21:22 +0800

    chore: 修改 eslint 规则，代码格式有问题时触发提示，保存时自动 prettier 格式化

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // eslint-disable-next-line prettier/prettier
   plugins: [
     vue(),
     vueJsx(),
