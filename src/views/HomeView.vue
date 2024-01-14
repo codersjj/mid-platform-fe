69d5d524a508e85c93e6b5341ecfe9331f69ca9c
codersjj <shanewestlife@outlook.com>
2024-01-14 22:30:59 +0800

    feat: 使用 pinia

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { watch } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useCounterStore } from '@/stores/counter'
+
+const counterStore = useCounterStore()
+
+const { count, doubleCount } = storeToRefs(counterStore)
+const { increment } = counterStore
+
+watch(count, (value, oldValue) => {
+  console.log(value, oldValue)
+})
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>HomeView</h1>
+    <button @click="increment">count is {{ count }}</button>
+    <button>doubleCount is {{ doubleCount }}</button>
   </main>
 </template>
